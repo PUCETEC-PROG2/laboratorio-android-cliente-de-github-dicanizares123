3f42198b68572033626a8d666426a56355734953
David Cañizares <davidusa1en1@gmail.com>
2025-11-14 15:07:38 -0500

    Finished

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,8 +1,15 @@
 package ec.edu.uisek.githubclient
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
+import ec.edu.uisek.githubclient.services.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,10 +20,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupRecycleView()
+        fetchRepositories()
     }
 
     private fun setupRecycleView(){
         reposAdapter = ReposAdapter()
         binding.repoRecyclerView.adapter = reposAdapter
+    }
+
+    private fun fetchRepositories(){
+        val apiService = RetrofitClient.gitHubApiService
+        val call = apiService.getRepost()
+
+        call.enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                if (response.isSuccessful) {
+                    val repos = response.body()
+                    if (repos != null) {
+                        reposAdapter.updateRepositories(repos)
+                    }else {
+                        showMessage("No se encontraron repositorios")
+                    }
+
+                } else  {
+                   val errorMsg = when (response.code()){
+                       401 -> "No autorizado"
+                       403 -> "Prohibido"
+                       404 -> "No encontrado"
+                        else -> "Error desconocido"
+                   }
+                    Log.e("MainActivity", errorMsg)
+                    showMessage(errorMsg)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                showMessage("Fallo en la red")
+                Log.e("MainActivity", "Fallo en la red ${t.message}")
+            }
+        })
+    }
+
+    private fun showMessage(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
